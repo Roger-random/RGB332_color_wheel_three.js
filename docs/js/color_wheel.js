c78cf1bec95ea4618837c91ec49694eea2a5005c
Roger Cheng <roger.random@outlook.com>
2021-04-28 01:18:25 -0700

    RGB332 to RGB888 upconvert

--- docs/js/color_wheel.js
+++ docs/js/color_wheel.js
@@ -10,6 +10,8 @@ document.body.appendChild( renderer.domElement );
 
 const orbitControl = new OrbitControls( camera, renderer.domElement );
 
+const axisZ = new THREE.Vector3(0,0,1);
+
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial( { color: 0x003300 } );
 var cube = new THREE.Mesh( geometry, material );
@@ -21,14 +23,38 @@ scene.add( cube );
 
 cube = new THREE.Mesh( geometry, material );
 cube.position.x = -2;
-scene.add( cube );
+
+var group = new THREE.Group();
+group.add( cube );
+group.rotateZ(0.1);
+scene.add(group);
 
 camera.position.z = 5;
 orbitControl.update();
 
+class rgb332 {
+  constructor(rgb332) {
+    this.color8 = rgb332;
+
+    var red = this.color8 >> 5;
+    this.redf = red/7;
+    this.red8 = this.redf * 0xFF;
+
+    var green = (this.color8 & 0x1C) >> 2;
+    this.greenf = green/7;
+    this.green8 = this.greenf * 0xFF;
+
+    var blue = this.color8 & 0x03;
+    this.bluef = blue/3;
+    this.blue8 = this.bluef * 0xFF;
+
+    this.color24 = this.red8 << 16 | this.green8 << 8 | this.blue8;
+  }
+}
+
 function animate() {
-    requestAnimationFrame( animate );
-    orbitControl.update();
-    renderer.render( scene, camera );
+  requestAnimationFrame( animate );
+  orbitControl.update();
+  renderer.render( scene, camera );
 }
 animate();
